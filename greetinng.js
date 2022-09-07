081ac40014cff9678ddded9c2fd3944bb9bfd422
Erlan Dzharkynbekov <omurbaev_erich@mail.ru>
2022-09-07 15:16:23 +0300

    сделал vse tri zadaniya

---
+++ greetinng.js
@@ -0,0 +1,2 @@
+// const names = prompt("Введите ваше имя: ")
+// if(names != null) alert("Привет " + names )
---
+++ svetofor.js
@@ -0,0 +1,20 @@
+var red = "red";
+var yellow = "yellow";
+var green = "green";
+
+const svetofor = prompt("Введите цвет: ")
+
+switch(svetofor){
+    case red:
+        alert("stop");
+        break;
+    case yellow:
+        alert("get ready");
+        break;
+    case green:
+        alert("GO");
+        break;
+        default:
+            alert("NO COLOR")
+
+}
